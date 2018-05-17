a127106899d019330a309ac89fafa8e049b32d20
TheBeardedRich <richardassheton-smith@hotmail.co.uk>
2018-05-17 20:08:56 +0100

    Added Simple Script to Follow Action with Camera

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    public Vector3 BallPos;
+    public Transform Ball;
+
+
+    // Update is called once per frame
+    public void Update () {
+
+        transform.position = Ball.position + BallPos;
+
+    }
+}
